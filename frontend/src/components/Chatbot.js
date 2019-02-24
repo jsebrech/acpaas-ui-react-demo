effb312c8711571557fde77407fdc6184c6ee447
Joeri Sebrechts <joeri.sebrechts@digipolis.be>
2019-02-24 21:53:05 +0100

    add chatbot widget

--- frontend/src/components/Chatbot.js
+++ frontend/src/components/Chatbot.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ChatbotWidget = window.auiEmbeddableWidgets.reactComponent(
+  // url to the definition
+  "https://widgets-o.antwerpen.be/definitions/chatbot.json",
+  { React, ReactDOM }
+)
 
 const chatbot = (prop) => {
 	return (
     <div className="row">
-      <div className="col-xs-12">
-        bla bla
+      <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <ChatbotWidget 
+          chatbot="demo" 
+          title="Demo" />
       </div>
     </div>
-	);
+  );
 }
 
 export default chatbot;
